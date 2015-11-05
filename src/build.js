5589f248351e1903ef81329cd155f91e90f80f1a
mennghao <296469203@qq.com>
2015-11-05 21:43:48 +0800

    update:fix

--- src/build.js
+++ src/build.js
@@ -1,10 +1,15 @@
 var fs = require('fs');
 
-
+/**
+ * [初始化静态资源]
+ * @param  {[type]} path [初始化资源路径]
+ * @param  {[type]} dist [description]
+ * @return {[type]}      [description]
+ */
 exports.init = function(path, dist){
 	fs.readdir(path, function(err, files){
 		if (err) { throw err; }
-		
+		//遍历目录中的文件
 		files.forEach(function(file){
 			var src = path + '/' + file,
 				build = dist + '/' + file,
