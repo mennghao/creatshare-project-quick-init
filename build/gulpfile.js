f81327a71743ec4d07046ad2b393a2a82ab5434c
mennghao <296469203@qq.com>
2015-11-21 21:47:16 +0800

    update:update config

--- build/gulpfile.js
+++ build/gulpfile.js
@@ -1,4 +1,6 @@
-var gulp = require('gulp'),
+'use strict'
+
+let gulp = require('gulp'),
 	less = require('gulp-less'),
 	concat = require('gulp-concat'),
 	uglify = require('gulp-uglify'),
@@ -6,11 +8,10 @@ var gulp = require('gulp'),
 	server = require('gulp-server-livereload'),
 	sourcemaps = require('gulp-sourcemaps'),
 	rename = require('gulp-rename'),
-	htmlreplace = require('gulp-html-replace');
+	htmlreplace = require('gulp-html-replace')
 
 //打包压缩css至测试目录
-gulp.task('test-bundle-css', function(){
-	return gulp.src('src/less/*.less')
+gulp.task('test-bundle-css', () => gulp.src('src/less/*.less')
 		.pipe(sourcemaps.init({loadMaps: true}))
 		.pipe(less())
 		.pipe(concat('style.css'))
@@ -20,37 +21,34 @@ gulp.task('test-bundle-css', function(){
 		.pipe(minify())
 		.pipe(sourcemaps.write())
 		.pipe(gulp.dest('src/dist/static/css/'))
-})
+)
 //打包压缩js至测试目录
-gulp.task('test-bundle-js', function(){
-	return gulp.src('src/js/*.js')
+gulp.task('test-bundle-js', () => gulp.src('src/js/*.js')
 		.pipe(concat('app.js'))
 		.pipe(rename({
 			suffix : '.min'
 		}))
 		.pipe(uglify())
 		.pipe(gulp.dest('src/dist/static/js/'))
-})
+)
 //转移图片资源至测试目录
-gulp.task('test-move-imgs', function(){
-	return gulp.src('src/imgs/*')
+gulp.task('test-move-imgs', () => gulp.src('src/imgs/*')
 		.pipe(gulp.dest('src/dist/static/imgs/'))
-})
+)
 //server
-gulp.task('webserver', function() {
-  gulp.src('./')
+gulp.task('webserver', () => gulp.src('./')
     .pipe(server({
       	livereload: {
 	        enable: true,
-	        filter: function(filePath, cb) {
+	        filter: (filePath, cb) => {
 	            cb( !(/node_modules/.test(filePath)) );
 	        }
 	    },
       	open: true
     }));
-})
+)
 //测试编译
-gulp.task('default', ['test-bundle-css', 'test-bundle-js', 'test-move-imgs', 'webserver'],function(){
+gulp.task('default', ['test-bundle-css', 'test-bundle-js', 'test-move-imgs', 'webserver'],() =>{
 	gulp.watch('src/less/*.less', ['test-bundle-css']);
 	gulp.watch('src/js/*.js', ['test-bundle-js']);
 	gulp.watch('src/imgs/*', ['test-move-imgs']);
@@ -59,8 +57,7 @@ gulp.task('default', ['test-bundle-css', 'test-bundle-js', 'test-move-imgs', 'we
 
 
 //打包css至发布目录
-gulp.task('dist-bundle-css', function(){
-	return gulp.src('src/less/*.less')
+gulp.task('dist-bundle-css', () => gulp.src('src/less/*.less')
 		.pipe(less())
 		.pipe(concat('style.css'))
 		.pipe(rename({
@@ -68,26 +65,24 @@ gulp.task('dist-bundle-css', function(){
 		}))
 		.pipe(minify())
 		.pipe(gulp.dest('dist/static/css/'))
-})
+)
 //打包js至发布目录
-gulp.task('dist-bundle-js', function(){
-	return gulp.src('src/js/*.js')
+gulp.task('dist-bundle-js', () => gulp.src('src/js/*.js')
 		.pipe(concat('app.js'))
 		.pipe(rename({
 			suffix : '.min'
 		}))
 		.pipe(uglify())
 		.pipe(gulp.dest('dist/static/js/'))
-})
+)
 //转移图片资源至发布目录
-gulp.task('dist-move-imgs', function(){
-	return gulp.src('src/imgs/*')
+gulp.task('dist-move-imgs', () => gulp.src('src/imgs/*')
 		.pipe(gulp.dest('dist/static/imgs/'))
-})
+)
 //正式环境
-gulp.task('dist', ['dist-bundle-css', 'dist-bundle-js', 'dist-move-imgs'], function(){
+gulp.task('dist', ['dist-bundle-css', 'dist-bundle-js', 'dist-move-imgs'], () => {
 	
-	var time = +new Date();
+	let time = +new Date()
 	
 	return gulp.src('./*.html')
 		.pipe(htmlreplace({
